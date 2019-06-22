3a3c60a820b73d65880e431ae129eac216d6c8f7
Dacihi Fukui <daichi.fukui.j@gmail.com>
2019-06-22 15:27:21 +0900

    #4 open file and tokenize text
    Signed-off-by: Dacihi Fukui <daichi.fukui.j@gmail.com>

--- extractor.py
+++ extractor.py
@@ -1,3 +1,13 @@
 """
 task : #4 TextデータからKeywordを抽出する
 """
+from janome.tokenizer import Tokenizer
+
+with open('data/text_sample',mode='r') as fp:
+    text = fp.read()
+
+t = Tokenizer()
+tokens = t.tokenize(text)
+
+for token in tokens:
+    print (token)
