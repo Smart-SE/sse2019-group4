a350905450de4efc8c8e9ea7963dbbc7238eed9f
da-okazaki <da-okazaki@outlook.com>
2019-06-28 16:49:02 +0900

    #5 US1,US2 完了

--- scraper.py
+++ scraper.py
@@ -1,12 +1,8 @@
 """
 Task    : #5 Keywordに対して、適切なページ（URL）を決定する
-version : 0.0.1
+version : 0.0.2
 author  : da-okazaki
 date    : 2019.06.22
-
-5-A. キーワードあり : ニュースのURLを取得
-5-B. キーワードなし : 一番ホットニュースのURLを取得
-
 """
 
 import feedparser
@@ -15,21 +11,27 @@ import os
 import json
 import pprint
 
-path = "./data/keyword.txt"
+input_path = "./data/keyword.txt"
+output_path = "./data/url.txt"
 
-# テキストから文字を抽出
-f = open(path)
-keyword = f.read()
-f.close()
+# ファイル存在チェック
+if (os.path.exists(input_path)):   
 
-# 1. ファイルの存在をチェック
-if (os.path.exists(path)):    
+    # テキストから文字を抽出
+    f = open(input_path)
+    keyword = f.read()
+    f.close()
+    
+    # 抽出後ファイル削除
+    os.remove(input_path)
 
-    # a. キーワードあり
+    # US1 (キーワードあり)
     if (keyword != "") :
-        # URLエンコーディング
+
+        # URL Encoding
         s_quote = urllib.parse.quote(keyword)
-        
+
+        # Google News Parse
         url = "https://news.google.com/news/rss/search/section/q/" + s_quote + "/" + s_quote + "?ned=jp&amp;hl=ja&amp;gl=JP"
         d = feedparser.parse(url)
         news = list()
@@ -45,13 +47,50 @@ if (os.path.exists(path)):
                 "sortkey": sortkey
             }
             news.append(tmp)
+            
+            if (tmp["no"] == 1) :
+                # OutputFile Create
+                f = open(output_path,'w')
+                # URL Description
+                f.write(entry.link)
+                f.close()
+                break
 
         news = sorted(news, key=lambda x: x['sortkey'])
         pprint.pprint(news)
 
-    # b. キーワードなし
-    elif (keyword == "") :
-        print("keywordなし")
 
-    # ファイル削除 ./data/keyword.txt
-    # ファイル作成 ./data/url.txt
+    # US2 (キーワード無し : TOPICS)
+    elif (keyword == "") :
+
+        # Google News Parse
+        url = "https://news.google.com/rss?hl=ja&gl=JP&ceid=JP:ja"
+        d = feedparser.parse(url)
+        news = list()
+        
+        for i, entry in enumerate(d.entries, 1) :
+
+            p = entry.published_parsed
+            sortkey = "%04d%02d%02d%02d%02d%02d" % (p.tm_year, p.tm_mon, p.tm_mday, p.tm_hour, p.tm_min, p.tm_sec)
+            tmp = {
+                "no": i,
+                "title": entry.title,
+                "link": entry.link,
+                "published": entry.published,
+                "sortkey": sortkey
+            }
+            news.append(tmp)
+
+            if (tmp["no"] == 1) :
+                # OutputFile Create
+                f = open(output_path,'w')
+                # URL Description
+                f.write(entry.link)
+                f.close()
+                break
+
+        news = sorted(news, key=lambda x: x['sortkey'])
+        pprint.pprint(news)
+
+else :
+    print ("file not exist")
